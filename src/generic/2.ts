681f5f3fa35071b7868546ac7e395b54d073db6d
NesterovSerhii <sergei_nesterov8@ukr.net>
2024-01-21 22:21:01 +0200

    generic

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,10 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+type TopObject = Pick<AllType, 'name' | 'position' | 'color' | 'weight'>;
+type BottomObject = Pick<AllType, 'name' | 'position' | 'color' | 'weight'>;
+
+function compare (top: TopObject, bottom: BottomObject): AllType {
   return {
     name: top.name,
     color: top.color,
